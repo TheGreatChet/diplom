6553988d3249a577d1e71b3c558039eeeef8377d
TheGreatChet <nicnamezicromain@gmail.com>
2022-05-04 07:06:14 +0300

    upd_04_05

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
-import './assets/css/App.scss'
-import Header from "./components/Header";
+import "./assets/css/App.scss";
+import Header from "./components/header/Header";
+import "./assets/images/search-background-1.jpg";
+import "./assets/images/search-background-2.jpeg";
+import "./assets/images/search-background-3.jpeg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <div class="search-container">
+        <div class="search">
+          <input class="search-input" type="text" placeholder="Введите ваш вопрос..."/>
+          <button class="search-btn" type="submit">
+            <FontAwesomeIcon icon={faSearch}/>
+          </button>
+        </div>
+        <div class="pic" id="pic3" />
+        <div class="pic" id="pic2" />
+        <div class="pic" id="pic1" />
+      </div>
     </div>
   );
 }
